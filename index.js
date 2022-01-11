08ac0bf984a3b186f04493af03e1d3b33d3533c3
nhuttu <nikohuttu1@gmail.com>
2022-01-11 14:37:22 +0200

    asd

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.put('/api/persons/:id', (req, res, next) => {
     })
     .catch(error => next(error))
 })
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
